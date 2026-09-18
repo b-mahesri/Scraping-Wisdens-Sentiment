7d7e7069ed820eaf3fa36e6cfb1ed1fe0d4ab83d
b-mahesri <bmahesri@gmail.com>
2026-09-18 14:25:09 -0400

    Create wisden_db.py
    Starting work on db

---
+++ wisden_db.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+
+# TODO: Create table
+# TODO: Write an insert function
+# TODO: Check what you need to do for the gitignore
+# TODO: Check if you need to create a separate file to store the db
+# TODO: Check if there are any other functions you should have
+
+
+# Reference Links:
+# SQLite Documentation: https://docs.python.org/3/library/sqlite3.html
